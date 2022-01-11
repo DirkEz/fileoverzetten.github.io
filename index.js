a5856e225b1bf04b81c56f68d91a546c70e5951c
DirkEz <66551001+DirkEz@users.noreply.github.com>
2022-01-11 15:51:53 +0100

    Filetransfer Script

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const formidable = require('formidable');
+const fs = require('fs');
+
+
+http.createServer(function(req, res) {
+
+    switch(req.url) {
+
+        default:
+            fs.readFile('index.html', function(err, data){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            })
+            break;
+
+        case '/fileupload':
+            fs.readFile('fileupload.html', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data)
+                res.end();
+            });
+            break;
+
+        case '/fileuploaded':
+            var form = new formidable.IncomingForm();
+            form.parse(req, function(err, fields, files) {
+                var oldpath = files.filetoupload.path;
+                var newpath = 'C:/Users/TC/Desktop/coding/nodejs/test1/downloaded/' + files.filetoupload.name; // <-- veranderen
+                fs.rename(oldpath, newpath, function(err){
+                    if(err) throw err;
+                    res.write('File uploaded and moved');
+                    res.end();
+                })
+            });
+            break;
+
+    };
+
+}).listen(8080);
